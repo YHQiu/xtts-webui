c96a393160657a7250a39e2d4a13ed0e40423ee0
qiuhongyu <qiuhongyu@fm.com>
2024-06-16 10:04:34 +0800

    gitignore

--- modules/__init__.py
+++
@@ -1,3 +0,0 @@
-import modules.text2voice
-import modules.voice2voice
-import modules.insturment_resemble
--- start_xtts_webui.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source venv/bin/activate
-
-python scripts/modeldownloader.py
-python app.py --deepspeed
-
-echo "Launch"
