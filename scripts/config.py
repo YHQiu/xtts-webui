9e7df1403c9e45e76e1e0b5278fed36193f7103b
qiuhongyu <qiuhongyu@taoliweilai.com>
2024-06-18 15:38:55 +0800

    feat

--- scripts/config.py
+++ scripts/config.py
@@ -3,13 +3,13 @@ speed_change_config = {
 }
 
 def get_speed_refactor(src_language, target_language):
-    # return 1.0
-    result = speed_change_config[target_language]
-    if result is None:
-        return 1.0
-    result = float(result)
-    print(result)
-    return result
+    return 1.0
+    # result = speed_change_config[target_language]
+    # if result is None:
+    #     return 1.0
+    # result = float(result)
+    # print(result)
+    # return result
 
 if __name__ == "__main__":
     print(1.0/get_speed_refactor(None, "en"))
